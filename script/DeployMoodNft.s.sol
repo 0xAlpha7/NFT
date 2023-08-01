b3d283d47437e0e40da99fa602a60c56c45a18bf
talha073 <talhajavedd@gmail.com>
2023-08-01 22:57:21 +0400

    refactor code

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {Script} from "forge-std/Script.sol";  
+import {Script, console} from "forge-std/Script.sol";  
 import {MoodNft} from "../src/MoodNft.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
     function run() external returns(MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+        
+        vm.startBroadcast();
+        MoodNft moodNFT = new MoodNft(svgToImageUri(sadSvg), svgToImageUri(happySvg)); 
+        vm.stopBroadcast();
+
+        return moodNFT;
+    
+
     }
 
     function svgToImageUri(string memory svg) public pure returns(string memory) {
